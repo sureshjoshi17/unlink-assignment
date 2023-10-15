a4e392c515b8ac8ac36be388c842ba42e1f19d82
Suresh Joshi <sureshjoshi17march@gmail.com>
2023-10-15 16:19:36 +0530

    Rate limit middlware added as global middlware for all routes to privent repeted request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const { rateLimit } = require('express-rate-limit');
 const app = express();
 const port = 8010;
 
@@ -26,5 +27,17 @@ db.serialize(() => {
     // attach swagger for api documentation
     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+    const limiter = rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes
+        limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+        standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+        legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+        // store: ... , // Use an external store for more precise rate limiting
+    })
+    
+    // Apply the rate limiting middleware to all requests
+    app.use(limiter)
+
     app.listen(port, () => logger.info(`App started and listening on port ${port}`));
 });
+
